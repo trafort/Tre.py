fa38d67902228786bb412cd8277b9a9522ef6bf3
trafort <34495933+trafort@users.noreply.github.com>
2017-12-28 21:08:44 -0500

    Update Bot.py

--- client/Bot.py
+++ client/Bot.py
@@ -3,3 +3,6 @@ import asyncio
 from discord.ext.commands import Bot
 from discord.ext import commands
 import platform
+
+# Bot Prefix, Pm_help=true, messges your help command. Pm_false does not
+client = Bot(description="", command_prefix="PUT YOUR PREFIX HERE", pm_help = True)
